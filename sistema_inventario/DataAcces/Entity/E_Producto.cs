9a36defb6bb20163037a4963156a1ee90a1fe544
SandovalM2002 <omar.sandoval74u@std.uni.edu.ni>
2022-04-03 19:25:49 -0600

    add procedures in visual

--- sistema_inventario/DataAcces/Entity/E_Producto.cs
+++ sistema_inventario/DataAcces/Entity/E_Producto.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DataAcces.Entity
 {
-    public class E_Producto
+    public class E_Producto : ConnectionToSQL
     {
         private int id_producto;
         private string nombre_producto;
@@ -39,9 +42,116 @@ namespace DataAcces.Entity
         public string Demanda_producto { get => demanda_producto; set => demanda_producto = value; }
         public float TasaM_producto { get => tasaM_producto; set => tasaM_producto = value; }
         public int StockSeguridad_producto { get => stockSeguridad_producto; set => stockSeguridad_producto = value; }
-        public bool estado_producto { get => estado_producto; set => estado_producto = value; }
-    
-        //METODOS DE LA BASE DE DATOS
 
+        public bool Estado { get => estado_producto; set => estado_producto = value; }
+
+
+        public bool estado_producto { get => estado_producto; set => estado_producto = value; }
+
+
+
+        public DataTable Search_product(string data)
+        {
+            DataTable res = new DataTable();
+            try
+            {
+                using (var conection = GetConnection())
+                {
+                    conection.Open();
+                    using (var Command = new SqlCommand())
+                    {
+                        Command.Connection = conection;
+
+                        Command.CommandText = "SP_VIEW_PRODUCT";
+                        Command.CommandType = CommandType.StoredProcedure;
+
+                        Command.Parameters.AddWithValue("@dato", data);
+
+                        SqlDataAdapter leer = new SqlDataAdapter(Command);
+                        leer.Fill(res);
+
+                        Command.ExecuteNonQuery();
+                        Command.Parameters.Clear();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return res;
+        }
+
+        public void Insert_product(E_Producto producto)
+        {
+            using (var conection = GetConnection())
+            {
+                conection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = conection;
+
+                    command.CommandText = "SP_INSERT_PRODUCT";
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    //Parametros del Procedimiento almacenado
+                    command.Parameters.AddWithValue("@nombre", producto.Nombre_producto);
+                    command.Parameters.AddWithValue("@costo", producto.Costo_producto);
+                    command.Parameters.AddWithValue("@precio", producto.Precio_producto);
+                    command.Parameters.AddWithValue("@exist", producto.Existencia_producto);
+                    command.Parameters.AddWithValue("ss", producto.StockSeguridad_producto);
+
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                }
+            }
+        }
+
+        public void Update_producto(E_Producto producto)
+        {
+            using (var conection = GetConnection())
+            {
+                conection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = conection;
+
+                    command.CommandText = "SP_UPDATE_PRODUCT";
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    //Parametros del Procedimiento almacenado
+                    command.Parameters.AddWithValue("@Id", producto.Id_producto);
+                    command.Parameters.AddWithValue("@nombre", producto.Nombre_producto);
+                    command.Parameters.AddWithValue("@costo", producto.Costo_producto);
+                    command.Parameters.AddWithValue("@precio", producto.Precio_producto);
+                    command.Parameters.AddWithValue("@exist", producto.Existencia_producto);
+                    command.Parameters.AddWithValue("ss", producto.StockSeguridad_producto);
+
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+                }
+            }
+        }
+
+        public void Unsubscribe_producto(int id)
+        {
+            using (var conection = GetConnection())
+            {
+                conection.Open();
+                using (var Command = new SqlCommand())
+                {
+                    Command.Connection = conection;
+
+                    Command.CommandText = "SP_DELETE_PRODUCT";
+                    Command.CommandType = CommandType.StoredProcedure;
+
+                    Command.Parameters.AddWithValue("@Id", id);
+
+                    Command.ExecuteNonQuery();
+                    Command.Parameters.Clear();
+                }
+            }
+        }
     }
-}
