41166f6805633ea778ae9fe304190a8d3e0732b9
SandovalM2002 <omar.sandoval74u@std.uni.edu.ni>
2022-05-17 08:29:05 -0600

    ddaagawe

---
+++ sql/Datos de la BD.sql
@@ -0,0 +1,9 @@
+/*tabla de stock*/
+INSERT INTO STOCK (nombre_s,tipo_s,costo_s,precio_p,existencia_s,stock_seg_s)  VALUES 
+('Producto X',1,100.00,800.00,40,0),
+('Material A',0,20.00,0.00,60,0),
+('Material B',0,8.00,0.00,20,0),
+('Material C',0,42.00,0.00,10,0),
+('Material D',0,2.00,0.00,20,0)
+
+/*Nodo*/
